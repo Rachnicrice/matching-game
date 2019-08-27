945350732748244a05e91279f7e0b66d074fdce0
Rachael Rice <Rachnicrice@gmail.com>
2019-08-27 14:07:45 -0700

    Fixed console log error

--- js/app.js
+++ js/app.js
@@ -81,31 +81,20 @@ function unflipClass () {
 
 //Function changes the class in the  first and second indexes of the array
 function rightCards() {
-  var choice1 = document.getElementById(clickedId[0]);
-  var choice2 = document.getElementById(clickedId[1]);
-  choice1.classList.add('correct');
-  choice2.classList.add('correct');
+  var choice1 = document.getElementById(`${clickedId[0]}`);
+  var choice2 = document.getElementById(`${clickedId[1]}`);
+  choice1.className('correct');
+  choice2.className('correct');
 }
 
 //Function changes the class back in the  first and second indexes of the array
 function resetCards() {
-
-  if (document.getElementById(clickedFramework[0].classList) === true) {
-    var choice1 = document.getElementById(clickedId[0]);
-    choice1.classList.replace('reset');
-  } else {
-    choice1 = document.getElementById(clickedId[0]);
-    choice1.classList.add('reset');
-  }
-
-  if (document.getElementById(clickedFramework[1].classList) === true) {
-    var choice2 = document.getElementById(clickedId[1]);
-    choice2.classList.replace('reset');
-  } else {
-    choice2 = document.getElementById(clickedId[1]);
-    choice2.classList.add('reset');
-  }
-
+  var choice1 = document.getElementById(`${clickedId[0]}`);
+  choice1.className('reset');
+  
+  var choice2 = document.getElementById(`${clickedId[1]}`);
+  choice2.className('reset');
+  
   unflipClass();
 }
 
