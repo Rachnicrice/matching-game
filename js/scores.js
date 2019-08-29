8f027d1ee88b151c51aabf3bc1f4fff25d33e837
Darrik Puetz <darrikpuetz@gmail.com>
2019-08-28 18:27:22 -0700

    storage to leaderboard

--- js/scores.js
+++ js/scores.js
@@ -5,7 +5,7 @@ function retrieveData() {
   for (var i = 0; i < scores.length; i++) {
     var scoreElement = document.createElement('li');
     var list = document.getElementById('myList');
-    scoreElement.textContent = scores[i].name[0] + ': ' + scores[i].time;
+    scoreElement.textContent = scores[i].name + ': ' + scores[i].time;
     console.log(scoreElement);
     list.appendChild(scoreElement);
   }
