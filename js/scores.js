624b03cad5d53d1fdefdf519d0e995633264c82e
Biniam <Biniamt23@yahoo.com>
2019-08-28 18:16:41 -0700

    finished leaderboard stuff

--- js/scores.js
+++ js/scores.js
@@ -1,6 +1,13 @@
 'use strict';
 
-function retrieveData () {
+function retrieveData() {
   var scores = JSON.parse(localStorage.getItem('final'));
+  for (var i = 0; i < scores.length; i++) {
+    var scoreElement = document.createElement('li');
+    var list = document.getElementById('myList');
+    scoreElement.textContent = scores[i].name[0] + ': ' + scores[i].time;
+    console.log(scoreElement);
+    list.appendChild(scoreElement);
+  }
 }
-
+retrieveData();
